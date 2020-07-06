ed69143bb2abddb8fa6d50b9cb88b67da68b1fb8
Zstacko <98705918+Zstacko@users.noreply.github.com>
2020-07-06 11:10:14 +0000

    Update azure-pipelines.yml for Azure Pipelines [skip ci]

--- CryptoContribute/Classes/ClosureProxy.swift
+++ CryptoContribute/Classes/ClosureProxy.swift
@@ -1,4 +1,8 @@
 import UIKit
 
 final class ClosureProxy {
-    let closure
+    let closure: () -> Void
+
+    init(attachTo: AnyObject, closure: @escaping () -> Void) {
+        self.closure = closure
+     
