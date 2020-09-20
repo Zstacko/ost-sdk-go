8551c6c9fed4e5955c2899609bd1f3120123904e
Zstacko <98705918+Zstacko@users.noreply.github.com>
2020-09-20 15:25:29 +0000

    Update Boilerplate

--- CryptoContribute/Classes/DonateView.swift
+++ CryptoContribute/Classes/DonateView.swift
@@ -38,4 +38,8 @@ final class DonateView: UIView {
             container.addSubview(button)
             button.edgeAnchors == container.edgeAnchors + UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
             container.widthAnchor == container.heightAnchor
-            stack.addArra
+            stack.addArrangedSubview(container)
+        }
+
+        stack.spacing = 12
+        stack.distribution = .fillEq
