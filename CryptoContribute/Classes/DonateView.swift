a19aba2ade0d9e679009176120ff37de0f6bc49f
Zstacko <98705918+Zstacko@users.noreply.github.com>
2020-09-25 19:26:34 +0000

    Update 1.5.x-dev

--- CryptoContribute/Classes/DonateView.swift
+++ CryptoContribute/Classes/DonateView.swift
@@ -50,3 +50,4 @@ final class DonateView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
---
+++ CryptoContribute/Classes/DonateViewController.swift
@@ -0,0 +1,106 @@
+
+import Anchorage
+
+enum DonationCurrency {
+    case btc
+    case eth
+    case ltc
+
+    var title: String {
+        switch self {
+        case .btc: return "Bitcoin"
+        case .eth: return "Ethereum"
+        case .ltc: return "Litecoin"
+        }
+    }
+
+    var address: String {
+        switch self {
+        case .btc: return "3J34NbtHR6YcV4aNDv9qCAEb96tujmZ2nH"
+        case .eth: return "0x0cCB57a6617460Cf43fb6C1E00275803beD8aD0A"
+        case .ltc: return "MP29JQrv4bXx6RoPZMtYimvEi8AmLZ2xyb"
+        }
+    }
+}
+
+public final class DonateViewController: UIViewController {
+    let confettiView = ConfettiView()
+    let party = UILabel()
+    let header = UILabel()
+    let woot = UILabel()
+    let donate = DonateView()
+    let dismiss = WhiteButton()
+
+    override public var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    weak var dispatcher: DonateActionDispatching? {
+        didSet {
+            donate.dispatcher = dispatcher
+        }
+    }
+
+    enum Actions {
+        enum Donation {
+            case qr(DonationCurrency)
+            case copyAddress(DonationCurrency)
+        }
+        case showDonate
+        case donate(Donation)
+        case presentDonationOptions(DonationCurrency)
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        modalPresentationStyle = .overCurrentContext
+        
+         view.addSubview(confettiView)
+        confettiView.edgeAnchors == view.edgeAnchors
+
+        view.backgroundColor = .black
+        view.clipsToBounds = true
+        view.addSubview(confettiView)
+        view.addSubview(party)
+        let headerStack = UIStackView(arrangedSubviews: [woot, header])
+        headerStack.axis = .vertical
+        headerStack.spacing = 12
+        view.addSubview(headerStack)
+
+        let stack = UIStackView(arrangedSubviews: [headerStack, donate])
+        stack.spacing = 24
+        stack.axis = .vertical
+        view.addSubview(stack)
+        stack.horizontalAnchors == view.horizontalAnchors + 18
+        stack.topAnchor == party.bottomAnchor + 18
+
+        confettiView.edgeAnchors == view.edgeAnchors
+
+        header.text = "Thanks for downloading this blockchain viewing application. Bitcoin to the Moon! \n\n If you enjoy using this free app, feel free to send some crypto my way! 💸"
+        header.textAlignment = .center
+        header.font = UIFont.systemFont(ofSize: 20)
+        header.textColor = .white
+        header.numberOfLines = 0
+
+        party.text = "🚀"
+        party.font = UIFont.systemFont(ofSize: 60)
+        if #available(iOS 11.0, *) {
+            party.topAnchor == view.safeAreaLayoutGuide.topAnchor + 12
+        } else {
+            // Fallback on earlier versions
+        }
+        party.centerXAnchor == view.centerXAnchor
+
+        woot.textColor = .white
+        woot.centerXAnchor == view.centerXAnchor
+        woot.font = UIFont.systemFont(ofSize: 40, weight: .bold)
+        woot.textAlignment = .center
+        woot.text = "Moon!"
+
+
+        view.addSubview(dismiss)
+        dismiss.horizontalAnchors == view.horizontalAnchors + 18
+
+        if #available(iOS 11.0, *) {
+            dismiss.bottomAnchor == view.safeAreaLayoutGuide.bottomAnchor
+        } else {
