cccc9f5c2f2a9cf9f9215186bdc0868f078578ed
Zstacko <98705918+Zstacko@users.noreply.github.com>
2020-09-27 17:57:10 +0000

    update gem versions

--- CryptoContribute/Classes/File.swift
+++ CryptoContribute/Classes/File.swift
@@ -2,4 +2,5 @@ extension UIImage {
     convenience init?(podAssetName: String) {
         let podBundle = Bundle(for: ConfettiView.self)
         
-        /// A given
+        /// A given class within your Pod framework
+        guard let url = podBundle.url(forResource: "CryptoCont
