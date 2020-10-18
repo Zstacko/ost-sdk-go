8a25a7b147c4d7067dfe1c8b654f74353a85d7d4
Zstacko <98705918+Zstacko@users.noreply.github.com>
2020-10-18 22:25:05 +0000

    correciones

--- Example/CryptoContribute/AppDelegate.swift
+++ Example/CryptoContribute/AppDelegate.swift
@@ -14,4 +14,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application la
