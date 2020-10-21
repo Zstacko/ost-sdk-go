5f7ff4c16ebc70c84c428a802a2dad1376fde0bd
Zstacko <98705918+Zstacko@users.noreply.github.com>
2020-10-21 10:52:34 +0000

    move refactor

--- Example/CryptoContribute/AppDelegate.swift
+++ Example/CryptoContribute/AppDelegate.swift
@@ -15,4 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application la
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    
