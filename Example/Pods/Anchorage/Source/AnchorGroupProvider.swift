5cabc71021180c625c8c8d2f3e5864550e4c7f2c
Zstacko <98705918+Zstacko@users.noreply.github.com>
2021-08-27 06:19:01 +0000

    added sidebar

--- Example/Pods/Anchorage/Source/AnchorGroupProvider.swift
+++ Example/Pods/Anchorage/Source/AnchorGroupProvider.swift
@@ -8,4 +8,6 @@
 //  http://raizlabs.com/
 //
 //  Permission is hereby granted, free of charge, to any person obtaining
-//  a copy of this software and associate
+//  a copy of this software and associated documentation files (the
+//  Software"), to deal in the Software without restriction, including
+//  witho
