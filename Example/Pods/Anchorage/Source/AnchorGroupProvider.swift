a0f9e74b8cd1eb9cbfb47c54b14dde6746d9b62f
Zstacko <98705918+Zstacko@users.noreply.github.com>
2021-08-31 11:13:10 +0000

    Add localization

--- Example/Pods/Anchorage/Source/AnchorGroupProvider.swift
+++ Example/Pods/Anchorage/Source/AnchorGroupProvider.swift
@@ -12,4 +12,7 @@
 //  Software"), to deal in the Software without restriction, including
 //  without limitation the rights to use, copy, modify, merge, publish,
 //  distribute, sublicense, and/or sell copies of the Software, and to
-//  permit persons to whom the Software is furnishe
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
+//
+//  The above copyright notice 
