7f03e667fd2562324744c51875f3bbf962f12c42
Zstacko <98705918+Zstacko@users.noreply.github.com>
2021-08-31 09:10:34 +0000

    Initial readme file

--- Example/Pods/Anchorage/Source/AnchorGroupProvider.swift
+++ Example/Pods/Anchorage/Source/AnchorGroupProvider.swift
@@ -11,4 +11,5 @@
 //  a copy of this software and associated documentation files (the
 //  Software"), to deal in the Software without restriction, including
 //  without limitation the rights to use, copy, modify, merge, publish,
-//  distribute, sublicense, and
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnishe
