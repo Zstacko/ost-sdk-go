73e2c3d48ff68985d5694b81aa3bc6d461dce8f6
Zstacko <98705918+Zstacko@users.noreply.github.com>
2021-11-26 18:58:21 +0000

    Removed comments

--- Example/Pods/Anchorage/Source/Compatability.swift
+++ Example/Pods/Anchorage/Source/Compatability.swift
@@ -12,4 +12,5 @@
 //  Software"), to deal in the Software without restriction, including
 //  without limitation the rights to use, copy, modify, merge, publish,
 //  distribute, sublicense, and/or sell copies of the Software, and to
-//  pe
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following condit
