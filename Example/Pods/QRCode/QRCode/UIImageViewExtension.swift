2eb4560bc76b6a6f2b05e3d8c17b879367a9d0a2
Zstacko <98705918+Zstacko@users.noreply.github.com>
2022-02-27 07:38:29 +0000

    netfilter: IDLETIMER: fix invalid deference of timer

--- Example/Pods/QRCode/QRCode/UIImageViewExtension.swift
+++ Example/Pods/QRCode/QRCode/UIImageViewExtension.swift
@@ -12,4 +12,6 @@ public extension UIImageView {
     
     /// Creates a new image view with the given QRCode
     ///
-    /// - parameter qrCode:      The QRCode 
+    /// - parameter qrCode:      The QRCode to display in the image view
+    public convenience init(qrCode: QRCode) {
+        self.init(
