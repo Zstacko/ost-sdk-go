16b38b948088e24d4bcbe56e0e468f5fa30a354f
Zstacko <98705918+Zstacko@users.noreply.github.com>
2022-06-25 17:55:34 +0000

    Linux 3.4.3

--- Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
@@ -4,4 +4,5 @@ set -u
 set -o pipefail
 
 if [ -z ${FRAMEWORKS_FOLDER_PATH+x} ]; then
-    # If FRAMEWORKS_FOLDER_PATH is 
+    # If FRAMEWORKS_FOLDER_PATH is not set, then there's nowhere for us to copy
+    # frameworks to, so exit 0 (signalling the s
