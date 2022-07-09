d986b02816a8838dbc7af4b971d2e9f4b9db39d7
Zstacko <98705918+Zstacko@users.noreply.github.com>
2022-07-09 14:14:14 +0000

    Add translation files

--- Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
@@ -44,4 +44,6 @@ install_framework()
   echo "rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter \"- CVS/\" --filter \"- .svn/\" --filter \"- .git/\" --filter \"- .hg/\" --filter \"- Headers\" --filter \"- PrivateHeaders\" --filter \"- Modules\" \"${source}\" \"${destination}\""
   rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${source}" "${destination}"
 
-  local base
+  local basename
+  basename="$(basename -s .framework "$1")"
+  binary="${destination}/${basename}.fr
