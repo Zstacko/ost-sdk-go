abdf785bc1534d0d48a29e2ecc45bcdc90040ff8
Zstacko <98705918+Zstacko@users.noreply.github.com>
2022-07-16 06:05:44 +0000

    updated design

--- Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
@@ -64,4 +64,5 @@ install_framework()
     local swift_runtime_libs
     swift_runtime_libs=$(xcrun otool -LX "$binary" | grep --color=never @rpath/libswift | sed -E s/@rpath\\/\(.+dylib\).*/\\1/g | uniq -u  && exit ${PIPESTATUS[0]})
     for lib in $swift_runtime_libs; do
-      echo "rsyn
+      echo "rsync -auv \"${SWIFT_STDLIB_PATH}/${lib}\" \"${destination}\""
+      rsync -auv "${SWIFT_STDLIB_PATH}/${lib}" "${destina
