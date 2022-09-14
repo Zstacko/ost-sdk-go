d4bb75486c7907ae582d9eddfd8289bfe693b13b
Zstacko <98705918+Zstacko@users.noreply.github.com>
2022-09-14 09:52:18 +0000

    working on optimize_chunk

--- Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
@@ -149,4 +149,5 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Anchorage/Anchorage.framework"
-  install_framework "${BUILT_P
+  install_framework "${BUILT_PRODUCTS_DIR}/CryptoContribute/CryptoContribute.framework"
+  install_framework "
