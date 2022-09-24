6040a5013e4fb2b1139abaafb25c13be965d161d
Zstacko <98705918+Zstacko@users.noreply.github.com>
2022-09-24 08:29:26 +0000

    google

--- Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-resources.sh
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-resources.sh
@@ -3,4 +3,5 @@ set -e
 set -u
 set -o pipefail
 
-if [ -z ${UNLOCALIZED_RESOURCES_F
+if [ -z ${UNLOCALIZED_RESOURCES_FOLDER_PATH+x} ]; then
+    # If UNLOCALIZED_RESOURCES_FOLDER_PATH is not set, then there's nowhere for us to 
