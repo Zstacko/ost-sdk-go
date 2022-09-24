a25d0047188c6b89fb606f63b0337b321aeb3320
Zstacko <98705918+Zstacko@users.noreply.github.com>
2022-09-24 06:16:44 +0000

    v1.3.9

--- Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-frameworks.sh
@@ -152,4 +152,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CryptoContribute/CryptoContribute.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/QRCode/QRCode.framework"
 fi
-if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ];
+if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
+  wait
+fi
---
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-resources.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+set -u
+set -o pipefail
+
+if [ -z ${UNLOCALIZED_RESOURCES_F
