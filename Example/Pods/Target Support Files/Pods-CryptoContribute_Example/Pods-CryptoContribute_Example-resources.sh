abdd51ef0aa29f1ec10e8ff42c0e4829b1888535
Zstacko <98705918+Zstacko@users.noreply.github.com>
2022-11-05 20:06:45 +0000

    V1.0

--- Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-resources.sh
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-resources.sh
@@ -65,4 +65,5 @@ EOM
       ;;
     *.framework)
       echo "mkdir -p ${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}" || true
-      mkdir -p "${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}
+      mkdir -p "${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      echo "rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" $RESOURCE_PATH ${TARGET_BUILD_DIR}/${FRAMEWOR
