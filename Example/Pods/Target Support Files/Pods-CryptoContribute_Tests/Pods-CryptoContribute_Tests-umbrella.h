1622fd156b54159f7d483bb228bdfeee1ff66052
Zstacko <98705918+Zstacko@users.noreply.github.com>
2023-02-05 11:36:53 +0000

    Prepare 0.27.0 release

--- Example/Pods/Target Support Files/Pods-CryptoContribute_Tests/Pods-CryptoContribute_Tests-umbrella.h
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Tests/Pods-CryptoContribute_Tests-umbrella.h
@@ -3,4 +3,12 @@
 #else
 #ifndef FOUNDATION_EXPORT
 #if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
 #define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_C
